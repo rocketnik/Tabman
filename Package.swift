f66e3771d557a893eb6bb2e9bc0914b870022ebe
Nikolas Mayr <rocketnik@login.n-mayr.de>
2023-12-19 21:44:43 +1300

    referring to Pageboy fix

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["Tabman"])
     ],
     dependencies: [
-        .package(url: "https://github.com/uias/Pageboy", from: "4.0.2")
+        .package(url: "https://github.com/rocketnik/Pageboy", from: "4.0.2+fork.nm.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
